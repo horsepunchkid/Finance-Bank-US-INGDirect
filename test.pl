66f9d288e60922668d5d8a794758b68b36a13ab5
Steven N. Severinghaus <sns@severinghaus.org>
2009-03-31 13:18:11 -0500

    Adding a test script

---
+++ test.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Finance::Bank::US::INGDirect;
+
+my %config = (
+    saver_id => '',
+    customer => '',
+    questions => [
+        'AnswerQ1.4' => '', # In what year was your mother born?
+        'AnswerQ1.5' => '', # In what year was your father born?
+        'AnswerQ1.8' => '', # What is the name of your hometown newspaper?
+    ],
+    pin => '',
+);
+
+my $ing = Finance::Bank::US::INGDirect->new(%config);
+
+use Finance::OFX::Parse::Simple;
+
+my $parser = Finance::OFX::Parse::Simple->new;
+my @txs = @{$parser->parse_scalar($ing->last_month_qfx)};
+my %accounts = $ing->accounts;
+
+for (@txs) {
+    my $account = $accounts{$_->{account_id}};
+    if($account) {
+        print "$account->{type}: $account->{nickname} - \$$account->{balance}\n";
+    }
+    else {
+        print "Account: $_->{account_id}\n";
+    }
+    printf "%s %-50s %8.2f\n", $_->{date}, $_->{name}, $_->{amount} for @{$_->{transactions}};
+    print "\n";
+}
+
